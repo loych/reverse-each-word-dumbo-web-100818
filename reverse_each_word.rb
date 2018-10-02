6235d4466815aa48375cc4486852617f3d6ba88e
IS Choi <insil321@hotmail.com>
2018-10-02 04:44:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentences)
   array=sentences.split(" ")
   new_array=[]
-  array.each do |sentence|
+  array.each do |sentences|
     new_array << sentences.reverse
 end
 new_array.join(" ")
